0fa4979f7e0f5ca7aff7340fb429d4628b7a70ad
thomasbouman1505 <65914841+thomasbouman1505@users.noreply.github.com>
2022-09-27 11:30:06 +0200

    inhoud van de website

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+ 
+
+  <link rel="stylesheet" href="css/style.css">
+  
+  <header>
+    <div class="wrapper">
+        <img src="image/logo.jpg" alt="logo" class="logo">
+        <H1>Opendag</H1>
+        
+    </div>
+  </header>
+  <body>
+    <div class="contentwrapper">
+    <div class="content">
+        <h1>Vakken</h1>
+        <img src="image/foto1.jpg" alt="foto1" class="foto1">
+      </div>
+      <div class="content">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+      </div>
+    </div>
+    <div class="contentwrapper">
+    <div class="content">
+      <h1>Je Rooster</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+    </div>
+      <div class="content">
+        <img src="image/foto2.png" alt="foto2" class="foto2">
+      </div>
+    </div>
+    <div class="contentwrapper">
+    <div class="content">
+      <h1>Leraren</h1>
+      <img src="image/leraar.jpg" alt="leraar" class="leraar">
+      </div>
+      <div class="content">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+    </div>
+  </div>
+    <div class="content">
+      <h1>wat kun je doen met je diploma</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+    </div>
+    <div class="contentwrapper2">
+    <div class="content2">
+      <h1>vaknties</h1>
+      <img src="image/vakantie.jpeg" alt="vakantie" class="vakantie">
+    </div>
+    <div class="content2">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+    </div>
+  </div>
+    <div class="contentwrapper1">
+    <div class="content1">
+      <h1>hoe zien de opdrachten er uit</h1>
+      <img src="image/opdracht.jpg" alt="opdracht" class="opdracht">
+    </div>
+    <div class="content1">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa as Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.sumenda! Amet, quas aliquid. necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+    </div>
+    </div>
+    <div class="content">
+      <h1>mijn mening over de opleiding</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur impedit perferendis architecto nulla ex veritatis sed necessitatibus illum officia aut velit delectus, fugiat sunt, in ipsa assumenda! Amet, quas aliquid.</p>
+    </div>
+  </body>
+
+ 
+<footer>
+  <div class="footer">
+  <p> © Curio | <a>Contact</a> | <a>Disclaimer</a> | <a>Privacy statement</a> | <a>Cookie instellingen</a> | <a>Sitemap</a> | Website: Panthera</p>
+</div>
+</footer>
+
+</html>
